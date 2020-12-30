120615ab6b8b785343a5ecc104d4a40a774d1327
leandro0104 <leandro.asepulveda@gmail.com>
2020-12-30 12:08:49 -0300

    Cambios para deploy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async( category ) =>{
 
     
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=ePT1QJI8VGgutE2FE7UdHXlcBLE0ba16`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=ePT1QJI8VGgutE2FE7UdHXlcBLE0ba16`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map(img => {
